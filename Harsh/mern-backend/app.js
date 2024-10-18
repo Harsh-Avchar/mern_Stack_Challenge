6d499e4bad6364ebf31d0ce1adecf17fee8ae48b
Harsh-Avchar <130824033+Harsh-Avchar@users.noreply.github.com>
2024-10-18 19:52:57 +0530

    Update app.js

--- Harsh/mern-backend/app.js
+++ Harsh/mern-backend/app.js
@@ -7,20 +7,19 @@ const transactionRoutes = require('./routes/transactions');
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB connection
 mongoose.connect('mongodb://localhost:27017/mern_stack', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Routes
+
 app.use('/api', transactionRoutes);
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
